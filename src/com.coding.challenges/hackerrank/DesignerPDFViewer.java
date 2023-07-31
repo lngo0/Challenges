d3a87f70cfe5eae4862c6804d700bb89ca21844c
lngo0 <longngo.gm@gmail.com>
2023-07-31 10:57:12 -0500

    HKR - remove extra class

--- src/com.coding.challenges/hackerrank/DesignerPDFViewer.java
+++ src/com.coding.challenges/hackerrank/DesignerPDFViewer.java
@@ -70,24 +70,3 @@ public class DesignerPDFViewer {
     }
 
 }
-
-class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        List<Integer> h = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                .map(Integer::parseInt)
-                .collect(toList());
-
-        String word = bufferedReader.readLine();
-
-        int result = DesignerPDFViewer.designerPdfViewer(h, word);
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedReader.close();
-        bufferedWriter.close();
-    }
-}
