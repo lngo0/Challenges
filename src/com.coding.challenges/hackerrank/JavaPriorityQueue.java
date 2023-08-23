18d8442b1277c612fdbb145f2749c5e3e527f942
lngo0 <longngo.gm@gmail.com>
2023-08-22 19:29:31 -0500

    Java Priority Queue

---
+++ src/com.coding.challenges/hackerrank/JavaPriorityQueue.java
@@ -0,0 +1,15 @@
+package com.coding.challenges.hackerrank;
+
+import java.util.PriorityQueue;
+
+public class JavaPriorityQueue {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        priorityQueue.add(4);
+        priorityQueue.add(2);
+        priorityQueue.add(1);
+        priorityQueue.add(3);
+
+
+    }
+}
