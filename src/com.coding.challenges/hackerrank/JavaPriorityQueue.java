0e6e5e6a4b7a7c19d4735b7518dc2cf69ea1ac6a
lngo0 <longngo.gm@gmail.com>
2023-08-23 14:53:51 -0500

    priority queue Java 15

--- src/com.coding.challenges/hackerrank/JavaPriorityQueue.java
+++ src/com.coding.challenges/hackerrank/JavaPriorityQueue.java
@@ -1,82 +1,51 @@
 package com.coding.challenges.hackerrank;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
 class StudentPQ {
-    private final int id;
-    private final String name;
-    private final double cgpa;
+    int token;
+    String name;
+    double cgpa;
 
-    public StudentPQ(int id, String name, double cgpa) {
-        this.id = id;
+    StudentPQ(int token, String name, double cgpa) {
+        this.token = token;
         this.name = name;
         this.cgpa = cgpa;
-    }
-
-    public int getID() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getCGPA() {
-        return cgpa;
-    }
-}
-
-class Priorities {
-
-    private final PriorityQueue<StudentPQ> queue = new PriorityQueue<>(
-            Comparator.comparing(StudentPQ::getCGPA).reversed()
-                    .thenComparing(StudentPQ::getName)
-                    .thenComparing(StudentPQ::getID));
-
-    public List<StudentPQ> getStudents(List<String> events) {
-        events.forEach((event) -> {
-            if (event.equals("SERVED")) {
-                queue.poll();
-            } else {
-                String[] details = event.split(" ");
-
-                queue.add(new StudentPQ(Integer.parseInt(details[3]), details[1], Double.parseDouble(details[2])));
-            }
-        });
-
-        List<StudentPQ> studentPQ = new ArrayList<>();
-        while (!queue.isEmpty()) {
-            studentPQ.add(queue.poll());
-        }
-
-        return studentPQ;
     }
 }
 
 public class JavaPriorityQueue {
-    private final static Scanner scan = new Scanner(System.in);
-    private final static Priorities priorities = new Priorities();
     public static void main(String[] args) {
-        int totalEvents = Integer.parseInt(scan.nextLine());
-        List<String> events = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
-        while (totalEvents-- != 0) {
-            String event = scan.nextLine();
-            events.add(event);
-        }
+        PriorityQueue<StudentPQ> pq = new PriorityQueue<>(
+                (s1, s2) -> (s1.cgpa != s2.cgpa) ? (Double.compare(s2.cgpa, s1.cgpa))
+                : (!s1.name.equals(s2.name) ? s1.name.compareTo(s2.name) : Integer.compare(s1.token, s2.token)));
 
-        List<StudentPQ> studentPQ = priorities.getStudents(events);
+        for (int i = 0; i < n; i++) {
+            String command = sc.next();
 
-        if (studentPQ.isEmpty()) {
-            System.out.println("EMPTY");
-        } else {
-            for (StudentPQ st: studentPQ) {
-                System.out.println(st.getName());
+            if (command.equals("ENTER")) {
+                String name = sc.next();
+                double cgpa = sc.nextDouble();
+                int token = sc.nextInt();
+
+                pq.offer(new StudentPQ(token, name, cgpa));
+            } else if (command.equals("SERVED")) {
+                pq.poll();
             }
         }
+
+        if (pq.isEmpty()) {
+            System.out.println("EMPTY");
+        } else {
+            while (!pq.isEmpty()) {
+                System.out.println(pq.poll().name);
+            }
+        }
+
+        sc.close();
     }
 }
