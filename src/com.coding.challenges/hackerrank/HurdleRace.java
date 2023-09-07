adff944788a8237bd793e4b6651f4a7f1876018a
lngo0 <longngo.gm@gmail.com>
2023-09-07 14:53:16 -0500

    hurdle race update

--- src/com.coding.challenges/hackerrank/HurdleRace.java
+++ src/com.coding.challenges/hackerrank/HurdleRace.java
@@ -1,11 +1,11 @@
 package com.coding.challenges.hackerrank;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class HurdleRace {
     /*
-        A video player plays a game in which the character competes in a hurdle race.
-        Hurdles are of varying heights, and the characters have a maximum height they can jump.
         There is a magic potion they can take that will increase their maximum jump height by 1 unit for each dose.
         How many doses of the potion must the character take to be able to jump all of the hurdles.
         If the character can already clear all of the hurdles, return 0.
@@ -61,5 +61,18 @@ public class HurdleRace {
             }
         }
         return dose;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(),
+                k = sc.nextInt();
+        sc.nextLine();
+        List<Integer> height = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            height.add(sc.nextInt());
+        }
+        sc.close();
+        System.out.println(hurdleRace(k, height));
     }
 }
